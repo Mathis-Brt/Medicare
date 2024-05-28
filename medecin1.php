935dd759b93b9139b6c0a0b5c413bf48ef4fe251
frankigoes6 <frank.braud@hotmail.com>
2024-05-28 20:10:38 +0200

    remplissage tout les info des medecin

--- medecin1.php
+++ medecin1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dr. Mathieu Basisse - Medicare</title>
+    <title>Docteur Medicare</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
