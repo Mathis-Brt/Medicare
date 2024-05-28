5a7e637f4996d4ebd9fea2adc9fea4db38bb21df
frankigoes6 <frank.braud@hotmail.com>
2024-05-28 19:41:26 +0200

    nv medecin genraliste

---
+++ medecine_generale.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Médecine générale - Tout parcourir - Medicare</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .button-container a:nth-child(2) {
+            background-color: lightblue; /* Changement de couleur du fond du bouton "Tout Parcourir" */
+        }
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <header class="header">
+        <div class="title-container">
+            <h1 style="font-size: 50px;"><span>Medicare:</span> Services Médicaux</h1>
+        </div>
+        <img src="logo_medicare2.png" alt="Medicare Logo" class="logo">
+        <img src="logo.png" alt="Logo Medicare" class="small-logo">
+    </header>
+    <nav class="navigation">
+        <div class="button-container">
+            <a href="accueil.html" class="button">Accueil</a>
+            <a href="tout_parcourir.html" class="button">Tout Parcourir</a>
+            <a href="#" class="button">Recherche</a>
+            <a href="#" class="button">Rendez-vous</a>
+            <a href="#" class="button">Votre compte</a>
+        </div>
+    </nav>
+    <main class="section">
+        <?php
+        // Afficher les erreurs PHP
+        error_reporting(E_ALL);
+        ini_set('display_errors', 1);
+
+        // Inclure le fichier PHP
+        include 'medecinG.php';
+        ?>
+    </main>
+    <footer class="footer">
+        <div class="contact-info">
+            <p>Téléphone: <a href="tel:+33 1 44 39 06 01">+33 1 44 39 06 01</a></p>
+            <p>Adresse: 10 Rue Sextius Michel, Paris, 75015</p>
+            <p>Email: <a href="mailto:omnes.medicare@gmail.com">omnes.medicare@gmail.com</a></p>
+        </div>
+        <div class="map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.6918020384956!2d2.2863122156753424!3d48.8512221792878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6701b4f58251b%3A0x167f5a60fb94aa76!2s10%20Rue%20Sextius%20Michel%2C%2075015%20Paris%2C%20France!5e0!3m2!1sen!2sus!4v1623867849655!5m2!1sen!2sus" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </footer>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
