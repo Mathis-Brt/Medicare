ef55fd2874e42ea4efdbb4261541ae78e76c920b
frankigoes6 <frank.braud@hotmail.com>
2024-05-31 18:34:37 +0200

    messagerie

---
+++ fetch_messages.php
@@ -0,0 +1,6 @@
+<?php
+// Ici, vous devriez récupérer les messages de votre base de données et les afficher
+// Ceci est juste un exemple pour simuler le chargement des messages
+echo "<div class='chat-message'>Bonjour comment je peux vous aider? </div>";
+
+?>
